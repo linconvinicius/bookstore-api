94b9ed5f8feda94e71b8e02a403209f196ee9fa9
linconvinicius <lvini_@live.com>
2024-12-12 18:10:17 -0300

    create query params

--- src/main/java/com/bookstore/jpa/repositories/BookRepository.java
+++ src/main/java/com/bookstore/jpa/repositories/BookRepository.java
@@ -1,4 +1,17 @@
 package com.bookstore.jpa.repositories;
 
-public interface BookRepository {
+import com.bookstore.jpa.models.BookModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface BookRepository extends JpaRepository<BookModel, UUID> {
+
+       BookModel findBookModelByTitle(String title);
+
+       @Query(value = "SELECT * FROM tb_book WHERE publisher_id = :1", nativeQuery = true)
+       List<BookModel> findBookModelByPublisherId(@Param("id") UUID id);
 }
